b7132fc2baa7eed266ed080f965a2d3f31f315ac
murquh1 <murquh1@lsu.edu>
2018-02-20 04:43:54 -0600

    js

---
+++ js/portfolio.js
@@ -0,0 +1,47 @@
+let project1ID = 1
+let project1URL = 'http://arcg.is/2GhrICS'
+let project1Title = 'Las Vegas Tour'
+let project1Thumb = true
+let project1webapp = true
+let project1form = false
+let project1MapExtent = [-115.2, 36.09, -115.12, 36.17]
+
+console.log('ID: ' + project1ID)
+console.log('URL: ' + project1URL)
+console.log('Title: ' + project1Title)
+console.log('Has thumbnail? ' + project1Thumb)
+console.log('Has web app? ' + project1webapp)
+console.log('Has user form? ' + project1form)
+console.log('Map extent: ' + project1MapExtent)
+
+let project2ID = 2
+let project2URL = 'https://arcg.is/14qj9v'
+let project2Title = 'Past Day Earthquakes'
+let project2Thumb = true
+let project2webapp = true
+let project2form = false
+let project2MapExtent = [-180, -180, 180, 180]
+
+console.log('ID: ' + project2ID)
+console.log('URL: ' + project2URL)
+console.log('Title: ' + project2Title)
+console.log('Has thumbnail? ' + project2Thumb)
+console.log('Has web app? ' + project2webapp)
+console.log('Has user form? ' + project2form)
+console.log('Map extent: ' + project2MapExtent)
+
+let project3ID = 3
+let project3URL = 'http://arcg.is/1nPq1e0'
+let project3Title = 'Report Dangerous Suspects'
+let project3Thumb = true
+let project3webapp = true
+let project3form = true
+let project3MapExtent = [-91.2, -30.39, -91.16, 30.42]
+
+console.log('ID: ' + project3ID)
+console.log('URL: ' + project3URL)
+console.log('Title: ' + project3Title)
+console.log('Has thumbnail? ' + project3Thumb)
+console.log('Has web app? ' + project3webapp)
+console.log('Has user form? ' + project3form)
+console.log('Map extent: ' + project3MapExtent)
