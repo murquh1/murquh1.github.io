3d21caecfa030dcaace3bc01be9086701e92ed05
murquh1 <murquh1@lsu.edu>
2018-02-23 20:05:48 -0600

    js04

--- js/code-assignment-04.js
+++ js/code-assignment-04.js
@@ -1,7 +1,7 @@
 let projectArray = [
   1,
   'http://arcg.is/2GhrICS',
-  'Las Vegas Tour'.
+  'Las Vegas Tour',
   'Map of famous Las Vegas locations',
   true,
   ['gambling', 'casino', 'hotels', 'vegas']
