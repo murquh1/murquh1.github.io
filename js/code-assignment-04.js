586551db2f10ccbc8cb1db3fc5d3eff7217ea63e
murquh1 <murquh1@lsu.edu>
2018-02-23 20:02:42 -0600

    js04

---
+++ js/code-assignment-04.js
@@ -0,0 +1,26 @@
+let projectArray = [
+  1,
+  'http://arcg.is/2GhrICS',
+  'Las Vegas Tour'.
+  'Map of famous Las Vegas locations',
+  true,
+  ['gambling', 'casino', 'hotels', 'vegas']
+]
+
+let projectObject = {
+  id: 1,
+  url: 'http://arcg.is/2GhrICS',
+  title: 'Las Vegas Tour',
+  desc: 'Map of famous Las Vegas locations',
+  thumb: true,
+  keywords: [
+    'gambling',
+    'casino',
+    'hotels',
+    'vegas'
+  ]
+}
+
+console.log(projectArray[5])
+console.log(projectObject.keywords)
+console.log(projectObject["keywords"])
