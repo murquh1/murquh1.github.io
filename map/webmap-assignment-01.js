2e0055b3fb84a834f46003693a6a41c7d5198ea1
Matthew Urquhart <murquh1@lsu.edu>
2018-04-03 10:33:51 -0500

    map

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -3,13 +3,8 @@ function map1 () {
   let mymap = L.map('map1').setView([51.505, -0.09], 13)
 
   // create basemap layer
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-      maxZoom: 18,
-      id: 'mapbox.streets',
-      accessToken: 'your.mapbox.access.token'
-  }).addTo(mymap);
-  
+  L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(mymap)
+
   // place a marker feature at a location
   let marker = L.marker([51.5, -0.09]).addTo(mymap)
 
